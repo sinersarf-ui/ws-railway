bf8aef10410988d1b4423b70dbc727b6a22ecf10
sinersarf-ui <sinersarf@gmail.com>
2026-01-08 21:26:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,15 @@
 const WebSocket = require("ws");
 
-// Koyeb akan inject PORT otomatis
 const PORT = process.env.PORT || 3000;
 
 const wss = new WebSocket.Server({ port: PORT });
 
-console.log("WSS WebSocket running on port", PORT);
+console.log("WebSocket running on port", PORT);
 
-wss.on("connection", (ws, req) => {
-  console.log("Client connected from", req.socket.remoteAddress);
+wss.on("connection", (ws) => {
+  ws.send("WSS Connected");
 
-  ws.send("Connected to WSS server");
-
-  ws.on("message", (message) => {
-    console.log("Received:", message.toString());
-    ws.send("Echo: " + message);
-  });
-
-  ws.on("close", () => {
-    console.log("Client disconnected");
+  ws.on("message", msg => {
+    ws.send("Echo: " + msg);
   });
 });
