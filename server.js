2f8499f72d7a348b1c3940634e249291014a27e2
sinersarf-ui <sinersarf@gmail.com>
2026-01-08 21:28:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const PORT = process.env.PORT || 3000;
 
 const wss = new WebSocket.Server({ port: PORT });
 
-console.log("WebSocket running on port", PORT);
+console.log("WSS running on port", PORT);
 
-wss.on("connection", (ws) => {
+wss.on("connection", (ws, req) => {
+  console.log("Client connected");
+
   ws.send("WSS Connected");
 
-  ws.on("message", msg => {
+  ws.on("message", (msg) => {
+    console.log("Received:", msg.toString());
     ws.send("Echo: " + msg);
   });
 });
